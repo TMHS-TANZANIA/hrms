a66867d357727ed225be77fa37bdf32bc3274556
jasjastone <jasjastone@gmail.com>
2026-06-05 11:08:45 +0300

    chore: fix the typo in create payroll entry

--- hrms/payroll/doctype/bulk_salary_assignment/bulk_salary_assignment.py
+++ hrms/payroll/doctype/bulk_salary_assignment/bulk_salary_assignment.py
@@ -171,11 +171,10 @@ class BulkSalaryAssignment(Document):
 		pe.title = self.title
 		pe.currency = self.currency
 		pe.payroll_payable_account = self.payroll_payable_account
-		pe.total_base = self.total_base
 		pe.total_variable = self.total_variable
-		pe.totoal_nhif = self.total_nhif
+		pe.total_nhif = self.total_nhif
 		pe.total_nssf = self.total_nssf
-		pe.total_helsb = self.total_helsb
+		pe.total_heslb = self.total_heslb
 
 		# Set posting date and payroll frequency
 		pe.posting_date = self.from_date
